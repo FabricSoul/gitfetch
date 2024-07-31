479348185b6717e329d8cebb42f4559fcc1c2cf9
GitHub Action <action@github.com>
2024-07-31 00:27:58 +0000

    Update package files to version 0.1.1

--- Formula/gitfetch.rb
+++ Formula/gitfetch.rb
@@ -2,7 +2,7 @@ class Gitfetch < Formula
   desc "A command-line tool to fetch and display Git contribution information"
   homepage "https://github.com/FabricSoul/gitfetch"
   url "https://github.com/FabricSoul/gitfetch/archive/0.1.1.tar.gz"
-  sha256 "f3c6fd92b87b310749ec140a689e47e6f00a05f2da710fd8dca9c359f63cc273"
+  sha256 "b0e1d961afacb8c91efbfaf3e709ddf14b7e7f9ab05582effa97759872e5063c"
   license "GPL-3.0"
 
   depends_on "rust" => :build
